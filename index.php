d6a762acc3fe34d7bad88a543eef1ba0e7280d34
Pablo <tdguchi@gmail.com>
2022-10-10 17:37:16 +0000

    index modificado

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-echo "Página principal<br>";
-echo "<a href='http://mtoffice.com/facturacion'>Ir al dashboard</a>";
+echo "<h1>Página principal</h1><br>";
+echo "<a  href='http://mtoffice.com/facturacion'>Ir al dashboard</a><br><br>";
+echo "<a href='?actualiza=true'>Actualizar BBDD</a>";
+
+if(isset($_GET['actualiza'])){
+        $server_name   = "localhost";
+        $username      = "root";
+        $database_name = "mtoffice";
+        
+        $cmd = "mysql -h {$server_name} -u {$username} {$database_name} < ./facturacion/mtoffice.sql";
+        exec($cmd);
+}
 ?>
