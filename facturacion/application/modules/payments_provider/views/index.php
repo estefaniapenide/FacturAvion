85681f36800dbca725edf43730d5c984756e9aef
Estefanía Penide <estefaniapenide@gmail.com>
2022-10-13 11:59:16 +0200

    Ver los pagos arreglado

--- facturacion/application/modules/payments_provider/views/index.php
+++ facturacion/application/modules/payments_provider/views/index.php
@@ -2,13 +2,13 @@
     <h1 class="headerbar-title"><?php _trans('payments'); ?></h1>
 
     <div class="headerbar-item pull-right">
-        <a class="btn btn-sm btn-primary" href="<?php echo site_url('payments/form'); ?>">
+        <a class="btn btn-sm btn-primary" href="<?php echo site_url('payments_provider/form'); ?>">
             <i class="fa fa-plus"></i> <?php _trans('new'); ?>
         </a>
     </div>
 
     <div class="headerbar-item pull-right">
-        <?php echo pager(site_url('payments/index'), 'mdl_payments'); ?>
+        <?php echo pager(site_url('payments_provider/index'), 'mdl_payments_provider'); ?>
     </div>
 
 </div>
@@ -18,7 +18,7 @@
     <?php $this->layout->load_view('layout/alerts'); ?>
 
     <div id="filter_results">
-        <?php $this->layout->load_view('payments/partial_payment_table'); ?>
+        <?php $this->layout->load_view('payments_provider/partial_payment_table'); ?>
     </div>
 
 </div>
