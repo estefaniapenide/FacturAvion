915cc000e12ae5fcd596a92583ea5322e689f206
Pablo <tdguchi@gmail.com>
2022-10-13 15:29:37 +0000

    arreglado filtro proveedores

--- facturacion/application/modules/clients/views/index.php
+++ facturacion/application/modules/clients/views/index.php
@@ -67,7 +67,7 @@
     <?php $this->layout->load_view('layout/alerts'); ?>
 
     <div id="filter_results">
-        <?php $this->layout->load_view('clients/partial_client_table',array('clients' => $records)); ?>
+        <?php $this->layout->load_view('clients/partial_client_table'); ?>
     </div>
 
 </div>
